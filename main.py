72938c603c93d2867b704e29abf6a69407771889
C04H18 <c04hn@teb.edu.pl>
2023-11-08 10:38:56 +0100

    Kompletna aplikacja rejestracji do lekarza na NFZ

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QDialog, QApplication
+
+
+class Myform(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi("layout.ui", self)
+        self.ui.rezerwoj.clicked.connect(self.rezerwacja)
+        self.show()
+
+    def rezerwacja(self):
+        imie = self.ui.odpImie.text()
+        nazwisko = self.ui.odpNazwisko.text()
+        specjalizacja=""
+        cena = 0
+        zailedniwizyta=0
+        if self.ui.internista.isChecked():
+            specjalizacja = "internista"
+        elif self.ui.pediatra.isChecked():
+            specjalizacja = "pediatra"
+        elif self.ui.dermatolog.isChecked():
+            specjalizacja = "dermatolog"
+        else:
+            specjalizacja = "internista"
+        if self.ui.czyprywatna.isChecked():
+            cena = 200
+            zailedniwizyta = int(random.uniform(0, 14))
+        else:
+            zailedniwizyta = int(random.uniform(0, 1000))
+        self.ui.wynikrezerwacji.setText("Pomyślnie zarezerwowano wizytę u lekarza: " + specjalizacja + ".\n Termin wizyty przypada za: " + str(zailedniwizyta) + ".\n Koszt wizyty: " + str(cena) + ". ")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Myform()
+    window.show()
+    sys.exit(app.exec())
+
+
